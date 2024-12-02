a6e52ca3395145b80ab88b7fc6babb996c2f72aa
potuitou <qpeng@risd.edu>
2024-12-02 14:33:28 -0500

    final

--- HW12_PASSWORD.ino
+++ HW12_PASSWORD.ino
@@ -1,9 +1,127 @@
-void setup() {
-  // put your setup code here, to run once:
+const int NUM_POTS = 3;
 
+int analogPins[NUM_POTS] = {A0, A1, A2};  
+const int redLED = D4;          
+const int greenLED = D3; 
+
+const int password[NUM_POTS] = {3, 4, 5}; 
+
+const int threshold = 10; 
+const int stableCountThreshold = 10; 
+
+// enum - 0,1,2
+enum PotState {IDLE, MOVING, STOPPED};
+PotState potStates[NUM_POTS];
+
+int inputs[NUM_POTS]; 
+int lastInputs[NUM_POTS]; 
+int stableCount[NUM_POTS];
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  digitalWrite(redLED, LOW);
+  digitalWrite(greenLED, LOW);
+
+  for (int i = 0; i < NUM_POTS; i++) {
+    int val = analogRead(analogPins[i]);
+    inputs[i] = map(val, 0, 10000 , 0, 9);
+    lastInputs[i] = inputs[i];
+    potStates[i] = IDLE;
+    stableCount[i] = 0;
+  }
+
+  // Initial password check
+  checkPassword();
+
+  Serial.print("Password: ");
+  for (int i = 0; i < NUM_POTS; i++) {
+    Serial.print(inputs[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool hasStopped = false;
 
+  for (int i = 0; i < NUM_POTS; i++) {
+    int val = analogRead(analogPins[i]);
+    inputs[i] = map(val, 0, 10000, 0 , 9);
+
+    if (inputs[i] != lastInputs[i]) {
+
+      //moving
+      potStates[i] = MOVING;
+      stableCount[i] = 0;
+
+    } else {
+
+      //is stable
+      if (potStates[i] == MOVING) {
+        stableCount[i]++;
+        if (stableCount[i] >= stableCountThreshold) {
+          // Potentiometer has stopped moving
+          potStates[i] = STOPPED;
+          hasStopped = true;
+        }
+      }
+    }
+
+    lastInputs[i] = inputs[i];
+
+    
+    // Serial.print(i);
+    // Serial.println(inputs[i]);
+    
+
+  }
+
+  if (hasStopped) {
+    checkPassword();
+
+    Serial.print("Password: ");
+    for (int i = 0; i < NUM_POTS; i++) {
+      Serial.print(inputs[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+
+    // Reset states to IDLE
+    for (int i = 0; i < NUM_POTS; i++) {
+      if (potStates[i] == STOPPED) {
+        potStates[i] = IDLE;
+      }
+    }
+  }
+
+  delay(50);
+}
+
+//check password
+void checkPassword() {
+
+  bool correct = true;
+
+  for (int i = 0; i < NUM_POTS; i++) {
+    if (inputs[i] != password[i]) {
+      correct = false;
+      break;
+    }
+  }
+
+  if (correct) {
+
+    Serial.println("Password Correct");
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(redLED, LOW);
+
+  } else {
+
+    Serial.println("Password Incorrect");
+    digitalWrite(greenLED, LOW);
+    digitalWrite(redLED, HIGH);
+    
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HW12_PASSWORD
- 
+
+Use 3 analog to input number from potentiometer to create a three digits password machine. I create three states, IDLE, MOVING, STOPPED. Therefore, it enbale the password only check the password when user has rateted the potentiometer, and there is no need for continously real-time checking.
+
+video: 
+https://drive.google.com/file/d/1bPKPn4VnIdddKDjzVbY7wnTCrk8THRGz/view?usp=sharing
+
+Circuit Sketch
+ ![Page I](./SKETCH.jpg)
